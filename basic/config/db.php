13e07b88e453db261708db726ed4837e8722630c
jonatan camarena <jhonee310@hotmail.com>
2015-12-07 13:38:50 -0700

    agregamos widget

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=tareas',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => 'brasilio89',
+    'password' => '',
     'charset' => 'utf8',
 ];
--- basic/config/web.php
+++ basic/config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'NvjBswGkPKJ494g6n2EyOR7PLBLlBOU-',
+            'cookieValidationKey' => 'lvcuVFUSqHE5Cf_V7DTokCzJjhoAsPyZ',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -38,6 +38,14 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        */
     ],
     'params' => $params,
 ];

537386c0d658475fab390b8f0f9bb9982765073f
oscararenas <oscar_skd@hotmail.com>
2015-12-07 12:51:44 -0700

    About Presentacion 2.0

--- views/site/about.php
+++ views/site/about.php
@@ -10,12 +10,21 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <h2 align="Center"> INSTITUTO TECNOLÓGICO DE TEPIC </h2>
+    <h1 align="Center"> INSTITUTO TECNOLÓGICO DE TEPIC </h1>
     
     <div align="center">
     <img class="transparent" alt="http://www.ittepic.edu.mx/images/escudo_itt_200x200.png" src="http://www.ittepic.edu.mx/images/escudo_itt_200x200.png">
     </div>
 
-    
+    <h1 align="center"> ADMINISTRADOR DE TAREAS</h1>
+    <h3 align="center"> Unidad 5</h3>
+
+    <h2 align="center"> Ingeniería en Sistemas Computacionales</h2>
+    <h3 align="center"> Programación Web</h3>
+    <h3 align="center">Oscar Alberto Arenas Gómez</h3>
+    <h3 align="center">Jonatan Omar Camarena Ortega</h3>
+    <h3 align="center">Isaac Sinoe Gutiérrez González</h3>
+    <h3 align="center">Alexis Hermosillo Becerra</h3>
+
 
 </div>

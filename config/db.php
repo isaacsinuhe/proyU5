ec71c4ac28112ea77559b5087483e2f0125ff4e8
jonatan camarena <jhonee310@hotmail.com>
2015-12-07 14:04:16 -0700

    se agrego el widget calendario a crear tarea en fecheinicio

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;port=3306;dbname=tareas',
     'username' => 'root',
-    'password' => 'brasilio89',
+    'password' => '1993',
     'charset' => 'utf8',
 ];
